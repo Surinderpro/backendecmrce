7deb034d25ab5786b6bf92f341769180e35d5d11
Surinder Jungral <114878952+Surinderpro@users.noreply.github.com>
2023-12-27 16:21:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,49 +28,49 @@ app.listen(PORT, () => {
 const productsToAdd = [
     {
         name: "Men Solid Round Neck Polyester Black T-Shirt",
-        imageUrl: "http://yourdomain.com/images/1.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/1.webp",
         price: 250,
         link: "https://fktr.in/ZTeWS64"
     },
     {
         name: "Men Typography Round Neck Cotton Blend Light Blue T-Shirt",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/2.webp",
         price: 199,
         link: "https://fktr.in/wVUQ8H4"
     },
     {
         name: "Men Colorblock Casual Jackete",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/2.webp",
         price: 499,
         link: "https://fktr.in/eRDfnEm"
     },
     {
         name: "Winter's Ultra Soft & Warm Neck Warmer & Cap",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/2.webp",
         price: 80,
         link: "https://fktr.in/6Nn93Bg"
     },
     {
         name: "Men Colorblock Casual Jacket",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/2.webp",
         price: 499,
         link: "https://fktr.in/K4A4dph"
     },
     {
         name: "Men Solid Casual Jacket",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/2.webp",
         price: 419,
         link: "https://fktr.in/Es9IeEu"
     },
     {
         name: "Men Colorblock Casual Jacket",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/2.webp",
         price: 499,
         link: "https://fktr.in/sl7ef9t"
     },
     {
         name: "Men Colorblock Casual Jacket",
-        imageUrl: "http://yourdomain.com/images/2.webp",
+        imageUrl: "https://ecommerce-ayq4.onrender.com/images/2.webp",
         price: 499,
         link: "https://fktr.in/sqinVAV"
     },
